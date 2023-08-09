57811c546b2f29847378a7cac87c00bdf09299d2
maximiliano836 <maxi10.02.00.g@gmail.com>
2023-08-09 17:40:25 -0300

    finalizado el problema

--- eventos.js
+++ eventos.js
@@ -1,10 +1,14 @@
 const constante = document.getElementById("container");
 const boton = document.getElementById("saludar");
 
-function alertaDiv() {
+function alertaDiv(event) {
   alert("Hola soy el div");
 }
 
 function alertaBoton() {
   alert("Hola soy el botón");
+  event.stopPropagation();
 }
+
+constante.addEventListener("click",alertaDiv);
+boton.addEventListener("click",alertaBoton);
